fff98af98427c1b12f4df17a501b3c5fd55bada5
Fares Kerkeni <93195495+Fares-Kerkeni@users.noreply.github.com>
2023-11-20 12:26:55 +0100

    changement de la nav

--- app/components/Nav/Nav.jsx
+++ app/components/Nav/Nav.jsx
@@ -1,32 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import styles from "./nav.module.scss";
 import Image from "next/image";
 import menu from "../../assets/menu.png";
-import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { chagevalue } from "../../store/slice/popupSlice";
 import "aos/dist/aos.css";
 import AOS from "aos";
+
 export default function Header() {
   const [state, setstate] = useState(false);
-  AOS.init();
+
+  useEffect(() => {
+    // Initialise AOS uniquement côté client
+    if (process.browser) {
+      AOS.init();
+    }
+  }, []);
+
   const handlePopup = () => {
     setstate(!state);
   };
+
   return (
     <main className={state ? styles.z_index : styles.main}>
       <div className={styles.nav_desktop}>
         <div className={styles.container_logo}>
           <p>L</p>
-          {/* <Image src={menu} alt="menu" width={40} height={40} /> */}
         </div>
         <div className={styles.container_link}>
           <Link href="/">Home</Link>
-          <Link href="/nos_service">Nos service</Link>
-          <Link href="/a_propos">A Propos</Link>
+          <Link href="/nos_service">Nos services</Link>
+          <Link href="/a_propos">À Propos</Link>
           <Link href="/galerie">Galerie</Link>
-          <Link href="/contact">contact</Link>
+          <Link href="/contact">Contact</Link>
         </div>
         <div></div>
       </div>
@@ -53,13 +60,12 @@ export default function Header() {
                   onClick={handlePopup}
                 />
               </div>
-
               <div className={styles.links}>
                 <Link href="/">Home</Link>
-                <Link href="/nos_service">Nos service</Link>
-                <Link href="/a_propos">A Propos</Link>
+                <Link href="/nos_service">Nos services</Link>
+                <Link href="/a_propos">À Propos</Link>
                 <Link href="/galerie">Galerie</Link>
-                <Link href="/contact">contact</Link>
+                <Link href="/contact">Contact</Link>
               </div>
             </div>
           </div>
