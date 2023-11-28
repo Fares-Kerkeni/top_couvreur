bb1d1c791e6096ca834337165314957a893909dc
Fares Kerkeni <93195495+Fares-Kerkeni@users.noreply.github.com>
2023-11-28 16:38:34 +0100

    form

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -11,34 +11,52 @@ export default function Page2() {
           <p>Envoyez nous un message</p>
         </div>
         <div className={styles.container_form}>
-          <form name="contact" method="POST" netlify>
-            <p>
-              <label>
-                Your Name: <input type="text" name="name" />
-              </label>
-            </p>
-            <p>
-              <label>
-                Your Email: <input type="email" name="email" />
-              </label>
-            </p>
-            <p>
-              <label>
-                Your Role:{" "}
-                <select name="role[]" multiple>
-                  <option value="leader">Leader</option>
-                  <option value="follower">Follower</option>
-                </select>
-              </label>
-            </p>
-            <p>
-              <label>
-                Message: <textarea name="message"></textarea>
-              </label>
-            </p>
-            <p>
-              <button type="submit">Send</button>
-            </p>
+          <form
+            name="contact"
+            method="POST"
+            dada-netlify="true"
+            onSubmit="submit"
+          >
+            <input type="hidden" name="form-name" value="contact" />
+            <input
+              className={styles.input}
+              type="text"
+              placeholder="Nom"
+              name="name"
+              for="name"
+            />
+            <input
+              className={styles.input}
+              type="text"
+              placeholder="Email"
+              name="email"
+              for="email"
+            />
+            <input
+              className={styles.input}
+              type="text"
+              placeholder="Téléphone"
+              name="phone"
+              for="phone"
+            />
+            <input
+              className={styles.input}
+              type="text"
+              placeholder="Sujet"
+              name="subject"
+              for="subject"
+            />
+
+            <textarea
+              className={styles.textarea}
+              type="text"
+              placeholder="Message"
+              name="message"
+              for="message"
+            />
+            <button type="submit" className={styles.button}>
+              Envoyer
+            </button>
           </form>
         </div>
       </div>
