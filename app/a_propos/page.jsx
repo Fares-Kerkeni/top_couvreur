7301e271c5d013b61cb0ffa314bdb9af9c68cd25
Fares Kerkeni <93195495+Fares-Kerkeni@users.noreply.github.com>
2023-12-02 16:48:31 +0100

    form

--- app/a_propos/page.jsx
+++ app/a_propos/page.jsx
@@ -1,8 +1,8 @@
 "use client";
 import styles from "./a_propos.module.scss";
 import Image from "next/image";
-import premiere_image from "../assets/construction.png";
-import deuxieme_image from "../assets/image.png";
+import premiere_image from "../assets/roofer-working-special-protective-work-600nw-1913665474.png";
+import deuxieme_image from "../assets/2b90b2683bcb.png";
 import Nav from "../components/Nav/Nav";
 import Footer from "../components/Footer/Footer";
 export default function Page2() {
@@ -16,7 +16,7 @@ export default function Page2() {
         <div className={styles.container_texte}>
           <div className={styles.block_text}>
             <p>
-              Top Toitures - Votre Artisan Couvreur de Confiance dans le Loiret
+              top couvreur - Votre Artisan Couvreur de Confiance dans le Loiret
             </p>
             <p>
               Spécialistes de la Couverture, nous vous offrons des services de
@@ -28,7 +28,7 @@ export default function Page2() {
           </div>
           <div>
             <div className={styles.block_text}>
-              <p>Jean Paul ROBERT - Engagé pour Votre Satisfaction</p>
+              <p> L’usure d’une toiture est inévitable</p>
               <p>
                 Depuis 2013, Jean Paul ROBERT place la satisfaction client au
                 cœur de notre engagement. Votre confiance est notre priorité
@@ -47,7 +47,7 @@ export default function Page2() {
           <div className={styles.block_text}>
             <p>SERVICE DE QUALITÈ </p>
             <p>
-              Top Toitures, votre expert en couverture dans le Loiret, se
+              top couvreur, votre expert en couverture dans le Loiret, se
               consacre à vous offrir des services de qualité depuis 2013.
               Spécialisés dans le domaine de la couverture, nous proposons un
               large éventail de prestations, notamment le ravalement,
@@ -56,12 +56,18 @@ export default function Page2() {
           </div>
           <div className={styles.block_text}>
             <p>
-              Top Toitures - Votre Artisan Couvreur de Confiance dans le Loiret
+              top couvreur - Votre Artisan Couvreur de Confiance dans le Loiret
             </p>
             <p>
-              Chez Top Toitures, nous adhérons à trois valeurs clés :
-              l'intégrité, la rigueur et la transparence. Votre satisfaction est
-              notre objectif premier, dépassant toute autre considération.
+              Rénovation partielle, Ce sont des travaux de remise en état d’une
+              couverture ne nécessitant pas le changement complet par du neuf.
+              Il s’agit donc de réparations ou de remplacements d’une partie de
+              la toiture afin d’améliorer son étanchéité ou son aspect
+              esthétique (un traitement hydrofuge des tuiles pouvant compléter
+              cette rénovation). En matière de rénovation partielle, nous
+              intervenons souvent sur les faîtages, arêtiers, tuiles de rives,
+              zinguerie, gouttières, fenêtres de toit, souches de cheminées
+              …etc.
             </p>
           </div>
         </div>
--- app/components/slider/slider.jsx
+++ app/components/slider/slider.jsx
@@ -8,6 +8,11 @@ import toiture_avant from "../../assets/image_ac/toiture_avant.jpg";
 import toiture1 from "../../assets/image_ac/toiture1.jpg";
 import toiture2 from "../../assets/image_ac/toiture2.jpg";
 import velux from "../../assets/image_ac/velux.jpg";
+import img1 from "../../assets/img_car/1.jpg";
+import img2 from "../../assets/img_car/2.jpg";
+import img3 from "../../assets/img_car/3.jpg";
+import img4 from "../../assets/img_car/4.jpg";
+import img5 from "../../assets/img_car/5.jpg";
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/navigation";
@@ -44,6 +49,21 @@ export default function App() {
         </SwiperSlide>
         <SwiperSlide>
           <Image src={velux} width={300} height={300} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image src={img1} width={300} height={300} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image src={img2} width={300} height={300} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image src={img3} width={300} height={300} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image src={img4} width={300} height={300} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image src={img5} width={300} height={300} />
         </SwiperSlide>
       </Swiper>
     </>
